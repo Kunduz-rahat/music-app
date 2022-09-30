620e5c8e8b425b8f380f2eeb63985ef485328b63
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-09-30 17:49:30 +0600

    add a functional of track

--- server/src/track/track.module.ts
+++ server/src/track/track.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { CommentSchema, Comment } from "./schemas/comments.schema";
+import { Track, TrackSchema } from "./schemas/track.schema";
 import { TrackController } from "./track.controller";
 import { TrackService } from "./track.service";
 
 
 @Module({
+	imports :[
+		MongooseModule.forFeature([{name:Track.name, schema:TrackSchema}]),
+		MongooseModule.forFeature([{name:Comment.name, schema:CommentSchema}])
+	],
 controllers:[TrackController],
 providers:[TrackService]
 })
