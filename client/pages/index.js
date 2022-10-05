74660620befb3d98f35700ba076fadaa16013b1d
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-10-05 19:22:49 +0600

    add navbar

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,30 @@
-
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 
 export default function Home() {
   return (
-    <div >
-    Home
+    <>
+    <Navbar/>
+    <div className="center">
+    <h1>Добро пожаловать!</h1>
+   <h4>Здесь собраны лучшие треки!</h4>
     </div>
+  <style jsx>
+  {`
+  .center {
+      margin-top:150px;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      
+     
+    `
+    
+  }
+
+  </style>
+    </>
   )
 }
